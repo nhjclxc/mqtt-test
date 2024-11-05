6016487d01166d756c4f02de2c09b90b8e39189b
LuoXianchao <nhjclxc@163.com>
2024-11-05 14:49:38 +0800

    新增一种实现

---
+++ spring-mqtt/subsribe/src/main/java/com/nhjclxc/subsribe/SubsribeApplication.java
@@ -0,0 +1,13 @@
+package com.nhjclxc.subsribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsribeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubsribeApplication.class, args);
+    }
+
+}
---
+++ spring-mqtt/subsribe/src/main/java/com/nhjclxc/subsribe/config/MQTTConfig.java
@@ -0,0 +1,92 @@
+package com.nhjclxc.subsribe.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(MQTTConfig.PREFIX)
+public class MQTTConfig {
+    //指定配置文件application-local.properties中的属性名前缀
+    public static final String PREFIX = "publish.mqtt";
+    private String host;
+    private String clientid;
+    private String username;
+    private String password;
+    private boolean cleansession;
+    private String default_topic;
+    private int timeout;
+    private int keepalive;
+    private int connectionTimeout;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getClientid() {
+        return clientid;
+    }
+
+    public void setClientid(String clientid) {
+        this.clientid = clientid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDefault_topic() {
+        return default_topic;
+    }
+
+    public void setDefault_topic(String default_topic) {
+        this.default_topic = default_topic;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public int getKeepalive() {
+        return keepalive;
+    }
+
+    public void setKeepalive(int keepalive) {
+        this.keepalive = keepalive;
+    }
+
+    public int getConnectionTimeout() {
+        return connectionTimeout;
+    }
+
+    public void setConnectionTimeout(int connectionTimeout) {
+        this.connectionTimeout = connectionTimeout;
+    }
+
+    public boolean isCleansession() {
+        return cleansession;
+    }
+
+    public void setCleansession(boolean cleansession) {
+        this.cleansession = cleansession;
+    }
+}
